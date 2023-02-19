3126d69dfadc07052d1db3ba989a023ac2880a6a
tbutc <91929683+tbutc@users.noreply.github.com>
2023-02-19 22:48:38 +0900

    Create 014.cpp

---
+++ 진현/2(1)/014.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int n; 
+    cin >> n;
+    
+    vector<int> heap;
+    heap.push_back(0); 
+    //heap은 1부터 시작하기 때문에 index=0인 부분 0으로 채워주기
+    vector<int> result;
+    
+    while(int i=0;i<n;i++){
+        int op;
+        cin >> op;
+        if(op==0){
+            result.push_back(heap[1]); //첫번째 heap 값 리턴
+        }
+        else{
+            int idx = heap.size(); //힙에 입력될 값의 index
+            heap.push_back(op);
+            while(op/2>0){ //부모 노드가 있을 때까지 반복
+                
+            }
+        }
+    }
+}
