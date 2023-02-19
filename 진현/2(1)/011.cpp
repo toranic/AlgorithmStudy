b279a4d6c9454c7a97637e2da6bd5ec8791a0784
tbutc <91929683+tbutc@users.noreply.github.com>
2023-02-19 21:22:46 +0900

    Update 011.cpp

--- 진현/2(1)/011.cpp
+++ 진현/2(1)/011.cpp
@@ -9,9 +9,7 @@ int main(){
     int n; 
     cin >> n;
     
-    stack<int> A; //0 초기화
-    int top=-1;
-    
+    stack<int> A;    
     vector<char> result;
     
     int nn=1; //push할 자연수
@@ -22,27 +20,27 @@ int main(){
     
     result.push_back(1);
     
-    for(i=0 ; i<n ; i++){
-        cin>>k;
-        while (nn <= k) {
-			result.push_back('+');
-			A.push(nn);
-			nn++;
-		}
-		if(k == A.top()) {
-			A.pop();
-			result.push_back('-');
-		}
-		else {
-			cout << "NO";
-			state = false;
-			return 0;
-		}
+    for(i=0 ; i<n ; i++){ //n회 입력받기
+        cin>>k; //입력받는 수 k
+        while (nn <= k) { //'push 대기중인 수 nn'이 k보다 커질 때까지 반복
+					result.push_back('+');
+					A.push(nn);
+					nn++;
+				}
+			if(k == A.top()) {
+				A.pop();
+				result.push_back('-');
+			}
+			else {
+				cout << "NO";
+				state = false;
+				return 0; //종료
+			}
     }
     
     if(state){
         for(i=1;i<result.size();i++){
-            cout<<result[i]<<"\n"; //질문하기 왜 1부터여야하는지?
+            cout<<result[i]<<"\n";
         }
     }
     return 0;
