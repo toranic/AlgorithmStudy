26a939de566dda987b929ae73e8673378b88e68a
tbutc <91929683+tbutc@users.noreply.github.com>
2023-02-20 03:17:46 +0900

    Update 011.cpp

--- 진현/2(1)/011.cpp
+++ 진현/2(1)/011.cpp
@@ -20,8 +20,6 @@ int main(){
     int k;
     bool state=true;
     
-    result.push_back(1);
-    
     for(i=0 ; i<n ; i++){ //n회 입력받기
         cin>>k; //입력받는 수 k
         while (nn <= k) { //'push 대기중인 수 nn'이 k보다 커질 때까지 반복
@@ -41,7 +39,7 @@ int main(){
     }
     
     if(state){
-        for(i=1;i<result.size();i++){ //질문하기!
+        for(i=0;i<result.size();i++){ //질문하기!
             cout<<result[i]<<"\n";
         }
     }
