69161f1a1d7c41d4683a53e881d7c67dc5690b31
tbutc <91929683+tbutc@users.noreply.github.com>
2023-02-20 03:13:17 +0900

    Update 014.cpp

--- 진현/2(1)/014.cpp
+++ 진현/2(1)/014.cpp
@@ -1,7 +1,21 @@
+//11286
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <queue>
 using namespace std;
+
+struct compare{
+    bool operator()(int o1, int o2){
+        int first_abs=abs(o1);
+        int second_abs=abs(o2);
+        if(first_abs == second_abs){
+            return o1>o2;
+        }
+        else{
+            return first_abs > second_abs;
+        }
+    }
+};
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -9,28 +23,19 @@ int main(){
     int n; 
     cin >> n;
     
-    vector<int> heap;
-    heap.push_back(0); 
-    //heap은 1부터 시작하기 때문에 index=0인 부분 0으로 채워주기
-    vector<int> result;
+    priority_queue<int, vector<int>,compare> MyQueue;
     
-    while(int i=0;i<n;i++){
-        int op;
-        cin >> op;
-        if(op==0){
-            result.push_back(heap[1]); //첫번째 heap 값 리턴
+    int op;
+    for(int i=0;i<n;i++){
+        cin>>op;
+        if(op){ //op!=0
+            MyQueue.push(op);
         }
-        else{
-            int idx = heap.size(); //힙에 입력될 값의 index
-            heap.push_back(op);
-            while(idx/2>0){ //부모 노드가 있을 때까지 반복
-                if(pow(heap[idx/2],2)>pow(op,2)){ 
-                //새로운 수가 절대값이 작은 경우
-                
-                }
-                else if(pow(heap[idx/2],2)>pow(op,2)){
-                    
-                }
+        else{ //op==0
+            if(MyQueue.empty())cout<<"0\n"; //emmpty
+            else{ //not empty
+                cout<<MyQueue.top()<<"\n";
+                MyQueue.pop();
             }
         }
     }
