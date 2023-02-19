3f0073e8b6c34146448f28eac9da1e7223326ac2
tbutc <91929683+tbutc@users.noreply.github.com>
2023-02-19 21:19:48 +0900

    Update 011.cpp

--- 진현/2(1)/011.cpp
+++ 진현/2(1)/011.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); //cout.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
     int n; 
     cin >> n;
