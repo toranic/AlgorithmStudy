423bb4a28b7932bed8d8ccb1bf809f81b54a97bf
hamo-o <yheel369@gmail.com>
2023-03-02 12:42:51 +0900

    023/BOJ/11724

--- 현영/Week2-2/2751.cpp
+++ 현영/Week2-2/2751.cpp
@@ -3,6 +3,24 @@
 #include <algorithm>
 using namespace std;
 
+int numbers[1000001];
+
+void swap(int a, int b){
+    int temp = numbers[a];
+    numbers[a] = numbers[b];
+    numbers[b] = temp;
+}
+
+void merge_sort(int start, int end) {
+    int mid = (start+end)/2;
+    merge_sort(start, mid);
+    merge_sort(mid+1, end);
+
+    for(int i=start; i<end; i++){
+        if()
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -10,11 +28,10 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int>numbers(n, 0);
     for(int i=0; i<n; i++)
         cin >> numbers[i];
 
-    sort(numbers.begin(), numbers.end());
+
 
     for(int i=0; i<n; i++)
         cout << numbers[i] << "\n";
