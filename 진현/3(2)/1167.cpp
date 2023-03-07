1f12a7aef4b3ea59b823866f03a50d423f0411ac
tbutc <91929683+tbutc@users.noreply.github.com>
2023-03-07 15:56:09 +0900

    Update 1167.cpp

--- 진현/3(2)/1167.cpp
+++ 진현/3(2)/1167.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-static queue <pair<int,int>> Q; //큐
+static queue <int> Q; //큐
 static vector <vector<pair<int,int>>> A; //인접 리스트
+static vector<bool> visited;
+static vector<int> max_val; //distance
+void BFS(int idx);
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,6 +17,7 @@ int main() {
 
     int n; //노드 개수
     cin>>n;
+    A.resize(n+1);
 
     for(int i=0; i<n;i++){
         int s, e, w; //start, end, weight
@@ -22,8 +27,45 @@ int main() {
             if(e==-1)break;
             cin>>w;
             A[s].push_back(pair<int,int>(e,w));
-            A[e].push_back(pair<int,int>(s,w));
+            //A[e].push_back(pair<int,int>(s,w));
         }
     }
 
+    max_val = vector<int>(n+1,0);
+    visited = vector<bool>(n+1,false);
+
+    BFS(1);
+    int max = 1;
+
+    for(int i=2 ; i<=n ; i++){
+        if(max_val[max] < max_val[i]){
+            max = i;
+        }
+    } //max 값 찾기
+
+    //cout<<A[max][0].first;
+    max_val = vector<int>(n+1,0);
+    visited = vector<bool>(n+1,false);
+
+    BFS(max);
+    sort(max_val.begin(),max_val.end());
+    cout<<max_val[n]<<"\n";
+
+}
+
+void BFS(int idx){
+    Q.push(idx);
+    visited[idx] = true;
+
+    while(!Q.empty()){
+        int now = Q.front();
+        Q.pop();
+        for(pair<int,int> i : A[now]){
+            if(!visited[i.first]){
+                visited[i.first]=true;
+                Q.push(i.first);
+                max_val[i.first]=max_val[now]+i.second;
+            }
+        }
+    }
 }
