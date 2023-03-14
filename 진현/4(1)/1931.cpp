04998634f6cde9a5f339d6a1d968378cf9b010dc
tbutc <91929683+tbutc@users.noreply.github.com>
2023-03-14 18:32:55 +0900

    Update 1931.cpp

--- 진현/4(1)/1931.cpp
+++ 진현/4(1)/1931.cpp
@@ -16,16 +16,21 @@ int main(){
     int fst, sec;
     for(int i=0 ; i<N ; i++){
         cin>>fst>>sec;
-        meeting.push(pair<int,int>(sec, fst));
+        meeting.push_back(pair<int,int>(sec, fst));
         //끝나는 시간, 시작 시간
     }
     
     sort(meeting.begin(), meeting.end()); //끝나는 시간 기준으로 정렬
     
     int result = 0;
+    int cur_end=0;
     
     for(int i=0 ; i<N ; i++){
-        
+        pair<int,int> tmp = meeting[i];
+        if(tmp.second >= cur_end){ //앞선 종료시간이 이번 시작시간보다 이르다면
+            cur_end = tmp.first; // 종료시간 갱신
+            result++;
+        }
     }
     
     cout<<result;
