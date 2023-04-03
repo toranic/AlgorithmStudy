6e4c73e1da6399bf0e955f5a8ae6eb8d66338e86
jkl0124 <hyjkl0124@naver.com>
2023-04-03 20:29:06 +0900

    no message

--- 현수/3-2/1300.cpp
+++ 현수/3-2/1300.cpp
@@ -9,33 +9,33 @@
 #include <memory.h>
 using namespace std;
 typedef long long ll;
-
+int arr[300];
+int maxx[300];
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll N, k;
-	cin >> N >> k;
-	ll end = N * N;
-	ll start = 1;
-	while (start <= end)
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		ll mid = (start + end) / 2;
-		ll cnt = 0;
-		for (int i = 1; i <= N; i++)
+		cin >> arr[i];
+	}
+	maxx[0] = arr[0];
+	maxx[1] = arr[1] + maxx[0];
+	for (int i = 2; i < N; i++)
+	{
+		if (maxx[i - 1] == arr[i - 1] + maxx[i - 2])
 		{
-			cnt += min(N, mid / i);
-		}
-		if (cnt >= k)
-		{
-			end = mid - 1;
+			maxx[i] = maxx[i - 2] + arr[i];
 		}
 		else
 		{
-			start = mid + 1;
+			maxx[i] = max(maxx[i - 2], maxx[i - 1]);
+			maxx[i] += arr[i];
 		}
 	}
-	cout << start;
+	cout << maxx[N - 1];
 	return 0;
 }
--- 현수/5/1747.cpp
+++ 현수/5/1747.cpp
@@ -9,48 +9,25 @@
 #include <memory.h>
 using namespace std;
 typedef long long ll;
-int arr[10000000];
-
-bool check(int n)
-{
-	int size = 0;
-	int temp[7];
-	while (n != 0)
-	{
-		temp[size] = n % 10;
-		n /= 10;
-		size++;
-	}
-	for (int i = 0; i <= size; i++)
-	{
-		if (temp[i] != temp[--size])
-		{
-			return false;
-		}
-	}
-	return true;
-}
+bool arr[1000001000000];
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int max = 10000000;
-	int N;
-	cin >> N;
-	for (int i = 2; i < 10000000; i++)
+	ll min, max;
+	cin >> min >> max;
+	int ans = max - min+1;
+	for (int i = 2; i * i <= max; i++)
 	{
-		if (arr[i] == 0)
-		{
-			if (N <= i&&check(i))
-			{
-				cout << i;
-				break;
-			}
-			for (ll j = i; j <= 10000000; j += i)
-				arr[j] = 1;
-		}
+		arr[i * i] = true;
 	}
+	for (int i = min; i <= max; i++)
+	{
+		if (arr[i] == true)
+			ans--;
+	}
+	cout << ans;
 	return 0;
 }
