ee3bc55b5a23e9e6c8f99aa5516141e315b86b8f
hamo-o <yheel369@gmail.com>
2023-04-04 20:14:43 +0900

    행렬/BOJ/10830/컴파일오류

--- 현영/Matrix/10830.cpp
+++ 현영/Matrix/10830.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 long long matrix[5][5];
-long long temp[5][5];
 int n, b;
 
-void mul(long long x[5][5], long long y[5][5]) {
+long long (*mul(long long x[5][5], long long y[5][5]))[5] {
+    long long temp[5][5];
     for(int f; f<n; f++){
         for(int s; f<s; f++){
             for(int j; f<j; f++)
@@ -16,18 +16,18 @@ void mul(long long x[5][5], long long y[5][5]) {
             temp[f][s] %= 1000;
         }
     }
+    return temp;
 }
 
-void divide(int size){
-    if (size == 1)
+long long (*divide(int sz))[5] {
+    if (sz == 1)
         return matrix;
 
-    long long temp[5][5] = {0};
-    &temp = divide(size/2);
-    if (size % 2 == 0)
-        return &square_matrix;
+    long long (*square_matrix)[5] = divide(sz/2);
+    if (sz % 2 == 0)
+        return square_matrix;
     else
-        return mul(mul(temp, temp), matrix)
+        return mul(mul(square_matrix, square_matrix), matrix);
 }
 
 int main() {
@@ -36,18 +36,21 @@ int main() {
 
     cin >> n >> b;
 
-    for(int i; i<n; i++) {
-        for(int j; j<n; j++)
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
             cin >> matrix[i][j];
     }
 
-    for(int i; i<n; i++) {
-        for(int j; j<n; j++)
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
             matrix[i][j] %= 1000;
     }
 
-    long long *answer[5][5];
-    answer = &divide(b);
-    for(int ans: answer)
-        print((" ").join(map(str, ans)));
+    long long (*answer)[5] = divide(b);
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
+            cout << answer[i][j] << " ";
+        cout << "\n";
+    }
 }
